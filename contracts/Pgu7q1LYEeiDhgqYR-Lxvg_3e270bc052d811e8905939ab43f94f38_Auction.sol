6175d777162a700217d03a5b31db26fd749b9e1f
greengeko <mackevicar@gmail.com>
2023-02-07 14:13:39 +0100

    added part 2 of submission

--- contracts/Pgu7q1LYEeiDhgqYR-Lxvg_3e270bc052d811e8905939ab43f94f38_Auction.sol
+++ contracts/Pgu7q1LYEeiDhgqYR-Lxvg_3e270bc052d811e8905939ab43f94f38_Auction.sol
@@ -104,7 +104,7 @@ contract Auction {
     //Hint : Use require to validate if "msg.sender" is equal to the "beneficiary".
     modifier onlyOwner {
         // ** Start code here. 2 lines approximately. **
-        
+        require(msg.sender == beneficiary);
         _;
         //** End code here. **
     }
@@ -130,7 +130,7 @@ contract Auction {
             you need to assign the address of the person obtained above to winners[id] */
 
             // ** Start coding here *** 1 line approximately.
-            
+            winners[id]=bidders[winnerId].addr;
                     
             //** end code here*
                 
